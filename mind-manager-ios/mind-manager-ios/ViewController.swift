eb691a8810c7d18585a9d6e323b462a7b160f4e1
Bruna Abatepaulo <bruna.abatepaulo@hotmail.com>
2021-03-05 15:54:34 -0300

    icon of the project

---
+++ mind-manager-ios/mind-manager-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mind-manager-ios
+//
+//  Created by Bruna Bispo on 05/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
