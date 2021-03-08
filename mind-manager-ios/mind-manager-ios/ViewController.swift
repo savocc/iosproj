2a9355ac26e397af50f71671ab775c3e2413cc7b
Bruna Abatepaulo <bruna.abatepaulo@hotmail.com>
2021-03-08 16:58:56 -0300

    home page and suggestions page layout

--- mind-manager-ios/mind-manager-ios/ViewController.swift
+++ mind-manager-ios/mind-manager-ios/ViewController.swift
@@ -12,7 +12,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.navigationController?.navigationBar.isHidden = true
     }
+    
+    
 
 
 }
