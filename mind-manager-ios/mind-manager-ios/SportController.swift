4773fcabd851c3fff3ced547c1597dfaa9a8b98c
evgeniya-zhukova <72122495+evgeniya-zhukova@users.noreply.github.com>
2021-03-28 23:34:25 +0300

    Comm

--- mind-manager-ios/mind-manager-ios/SportController.swift
+++ mind-manager-ios/mind-manager-ios/SportController.swift
@@ -14,6 +14,7 @@ class SportController: UIViewController {
     @IBOutlet weak var lblSport: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        //for now using moodySport
         let control = suggestionsController.moodySport.randomElement();
         lblSport.text = control;
      }
